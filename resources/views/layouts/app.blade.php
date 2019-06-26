761dd6c4b9807da7f6762c0fa7f18456560699bc
Goshawk <mjone352@gmail.com>
2019-06-27 05:54:22 +0800

    css fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Main CSS-->
     <link rel="stylesheet" type="text/css" href="{{asset('template/css/main.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/jQuery.fileupload.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('template/css/jQuery.fileupload.css')}}">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
@@ -35,8 +35,8 @@
     <script src="{{asset('template/js/main.js')}}"></script>
     <!-- The javascript plugin to display page loading on top-->
     <script src="{{asset('template/js/plugins/pace.min.js')}}"></script>
-    <script src="{{asset('js/bootstrap-notify.min.js')}}"></script>
-    <script src="{{asset('js/sweetalert.min.js')}}"></script>
+    <script src="{{asset('template/js/bootstrap-notify.min.js')}}"></script>
+    <script src="{{asset('template/js/sweetalert.min.js')}}"></script>
     <!-- Page specific javascripts-->
     <script>
         $.ajaxSetup({
