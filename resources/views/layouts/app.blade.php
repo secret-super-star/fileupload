42b1a4786b24e964095d2390a4c3eb9e702c90ec
Goshawk <mjone352@gmail.com>
2019-06-27 06:10:06 +0800

    dd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Main CSS-->
     <link rel="stylesheet" type="text/css" href="{{asset('template/css/main.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('template/css/jQuery.fileupload.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('template/css/jquery.fileupload.css')}}">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
