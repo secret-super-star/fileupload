8938ddb20f21dd9327d06ed91ec79f8a32f84560
Goshawk <mjone352@gmail.com>
2019-06-28 02:03:42 +0800

    fix bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,27 +26,35 @@
             <thead>
               <tr>
                 <th style="width: 20px">No</th>
+                <th>Created</th>
                 <th>Title</th>
                 <th>Real Title</th>
                 <th>Path</th>
-                <th>Created</th>
                 <th style="width: 70px">Copy Url</th>
-                <th style="width: 50px">Delete</th>
+                @if(Auth::user()->role_id == 1)
+                  <th style="width: 50px">Delete</th>
+                @else
+                @endif
+
               </tr>
             </thead>
             <tbody>
                 @foreach ($videos as $key=>$video)
                 <tr>
                   <td>{{++$key}}</td>
+                  <td>{{date("d/m/Y",strtotime($video->created_at))}}</td>
                   <td>{{$video->title}}</td>
                   <td>{{$video->real_title}}</td>
                   <td>{{$video->path}}</td>
-                  <td>{{date("Y-m-d",strtotime($video->created_at))}}</td>
                   <td>
                     <input type="hidden" name="" id="copy_path" value="{{$video->path}}">
                     <button class="btn btn-success btn-sm copy_button" type="button">Copy URL</button>
                   </td>
-                  <td><a class="btn btn-danger btn-sm text-white" href="{{ route('delete', $video->id) }}">Delete</a></td>
+                  @if(Auth::user()->role_id == 1)
+                    <td><a class="btn btn-danger btn-sm text-white" href="{{ route('delete', $video->id) }}">Delete</a></td>
+                  @else
+                  @endif
+
                 </tr>
                 @endforeach
 
@@ -65,15 +73,7 @@
 <script src="{{asset('template/js/plugins/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('template/js/plugins/dataTables.bootstrap.min.js')}}"></script>
 <script type="text/javascript">
-  $('#sampleTable').DataTable({
-    "columnDefs": [ {
-
-    "targets": [5,6], // column or columns numbers
-
-    "orderable": false,  // set orderable for selected columns
-
-    }],
-  });
+  $('#sampleTable').DataTable();
 
   $('.copy_button').click(function(){
     var temp_el = $("<input>");
